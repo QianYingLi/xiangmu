413872d2aaade397d6f20218ed674d82cfcd1100
QianYingLi <45376167+QianYingLi@users.noreply.github.com>
2019-02-12 21:59:46 +0800

    第二天

--- js/zhuye.js
+++ js/zhuye.js
@@ -1,7 +1,73 @@
 //下拉列表
+//left
 $(".lc").mouseenter(function(){
 	$(".inset").css("display","block");
+}).mouseleave(function(){
+
 })
+$(".inset").mouseenter(function(){
+	$(".inset").css("display","block");
+}).mouseleave(function(){
+	$(".inset").css("display","none");
+})
+$(".inset").find("a").mouseenter(function(){
+	$(this).css("background-color","#E2A446").parent().siblings().find("a").css("background-color","white");
+		$(this).css("background-color","#E2A446").siblings().find("a").css("background-color","white");
+})
+$(".inset").find("a").click(function(){
+	if($(this).html()=="渝中区"||$(this).html()=="重庆"){
+		$(".lc").html($(".lc").html());
+	}else{
+		$(".lc").html($(this).html());
+	}
+})
+
+$(".left a").mouseenter(function(){
+	$(this).css("color","orange");
+}).mouseleave(function(){
+	$(this).css("color","black");
+})
+$(".right a").mouseenter(function(){
+	$(this).css("color","orange");
+}).mouseleave(function(){
+	$(this).css("color","black");
+})
+
+//right
+$(".ec1").mouseenter(function(){
+		   $(".order").show();
+	})
+	$(".order").mouseleave(function(){
+		$(this).hide();
+	})
+	
+$(".cl").mouseenter(function(){
+		$(".collect").css("display","block");
+	})
+	$(".cl").mouseleave(function(){
+		$(".collect").css("display","none");
+	})
+	$(".pho").mouseenter(function(){
+		$(".img").css("display","block");
+	})
+	$(".pho").mouseleave(function(){
+		$(".img").css("display","none");
+	})
+//活动
+window.onscroll=function(){
+	var sTop=document.documentElement.scrollTop||document.body.scrollTop;
+	if(sTop>10&&sTop<200){
+		huodong.style.display="block";
+	}else{
+		huodong .style.display="none";
+	}
+}
+//gotop.onclick = function(){
+// 		document.documentElement.scrollTop = document.body.scrollTop = 0;
+// 		
+// 		gotop.style.display = "none";
+// 	}
+
 
 //轮播图
 var timer=null;
